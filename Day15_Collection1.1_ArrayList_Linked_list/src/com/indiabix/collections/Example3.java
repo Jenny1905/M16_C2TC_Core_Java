4bfcbc7d879ee00d501c741b6fa5e899c5257f18
HP <HP@LAPTOP-KPAN81EK>
2022-04-28 16:23:56 +0530

    First Commit

---
+++ Day15_Collection1.1_ArrayList_Linked_list/src/com/indiabix/collections/Example3.java
@@ -0,0 +1,20 @@
+package com.indiabix.collections;
+
+import java.util.ArrayList;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList<>();
+		Object clone1;
+		a.add("Hrithik");
+		a.add("Haresh");
+		a.add(5);
+		System.out.println(a);
+		clone1=a.clone();
+		System.out.println(clone1);
+		
+
+	}
+
+}
